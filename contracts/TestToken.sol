af7ab0d8139f626c26ac56822d144abc519d226f
Semi Invader <=>
2023-05-09 21:20:29 -0600

    fix code and improve coverage

--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -5,7 +5,21 @@ pragma solidity 0.8.17;
 import "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol";
 
 contract TestToken is ERC20PresetFixedSupply {
+    mapping(address => bool) public forbidden;
+
     constructor()
         ERC20PresetFixedSupply("TestToken", "TT", 100000000 ether, msg.sender)
     {}
+
+    function _beforeTokenTransfer(
+        address from,
+        address to,
+        uint amount
+    ) internal override {
+        require(!forbidden[to], "Forbidden");
+    }
+
+    function setForbidden(address user) external {
+        forbidden[user] = true;
+    }
 }
